9edb43a8bfe820f0ef208d0664f46d07291acacd
gagm2737 <mathgag555@hotmail.com>
2016-08-15 14:32:17 -0400

    Renommer fichier

--- standardizeDebitmetre.R
+++
@@ -1,4 +0,0 @@
-#Data Frame containing all data
-allData = read.csv("C:/Users/gagm2737/Documents/SpiderOak Hive/DONNEES_FILAIRES/WORKINPROGRESS/7. Sans ID ni T_base.csv")
-
-expData = allData[grep(expList[i], allData$No_Test),]
