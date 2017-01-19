a31ab0e0aff021b249e91595f0bfb23ef6292787
gagm2737 <mathgag555@hotmail.com>
2017-01-19 16:05:21 -0500

    Ajout variables input/ouputFile Ajout colNames qu'une fois au début au lieu d'à chaque itératon

--- countNumberOfTriggers.R
+++ countNumberOfTriggers.R
@@ -1,8 +1,13 @@
 #Data Frame containing all data
-allData = read.csv("C:/Users/gagm2737/Documents/SpiderOak Hive/DONNEES_FILAIRES/WORKINPROGRESS/7. Sans ID ni T_base.csv")
+inputFile = "C:/Users/gagm2737/Documents/SpiderOak Hive/DONNEES_FILAIRES/WORKINPROGRESS/outputDebitmetre.csv"
+outputFile = "C:/Users/gagm2737/Documents/SpiderOak Hive/DONNEES_FILAIRES/WORKINPROGRESS/output_nbTriggers.csv"
+
+allData = read.csv(inputFile)
 
 #Vector containing all EXP names
-expList <- c('EXP18', 'EXP21', 'EXP22', 'EXP23', 'EXP24', 'EXP25', 'EXP26', 'EXP27', 'EXP28', 'EXP29', 'EXPDTA16', 'EXPDTA17', 'EXPDTA18', 'EXPDTA19', 'EXPDTA20', 'EXPMCI16', 'EXPMCI17', 'EXPMCI18', 'EXPMCI19', 'EXPMCI20', 'EXPMCI21', 'EXPMCI23', 'EXPMCI24', 'EXPMCI25', 'EXPMCI26')
+expList <- c('EXP18', 'EXP21', 'EXP22', 'EXP23', 'EXP24', 'EXP25', 'EXP26', 'EXP27', 'EXP28', 'EXP29', 'EXPDTA16', 'EXPDTA17',
+             'EXPDTA18', 'EXPDTA19', 'EXPDTA20', 'EXPMCI16', 'EXPMCI17', 'EXPMCI18', 'EXPMCI19', 'EXPMCI20', 'EXPMCI21',
+             'EXPMCI23', 'EXPMCI24', 'EXPMCI25', 'EXPMCI26')
 
 #Declare endData, a data frame containing final exit data (nb trigger of all sensors for all EXPs)
 endData <- data.frame(matrix(ncol = length(allData[1,]), nrow = length(expList)))
@@ -35,12 +40,12 @@ for (i in 1:length(expList)){
     counterList[grep(sensor, sensorList) + 1] <- counter
   }
   
-  print(counterList)
+  #print(counterList)
   
   #Copy counterList into endData for all EXPs
   endData[i,] <- counterList
   
   #Output in csv format
-  write.csv(endData, file = "C:/Users/gagm2737/Documents/SpiderOak Hive/DONNEES_FILAIRES/WORKINPROGRESS/output.csv", row.names = FALSE )
+  write.csv(endData, file = outputFile, row.names = FALSE )
 
 }
