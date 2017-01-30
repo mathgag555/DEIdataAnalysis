c9bd9ec5e9c51fb3dcef90fd67e380d10b98e71e
gagm2737 <mathgag555@hotmail.com>
2017-01-30 16:36:16 -0500

    Changements mineurs, enlever commentaires inutiles

--- convertDataToEvents.R
+++ convertDataToEvents.R
@@ -1,27 +1,33 @@
 #Data Frame containing all data
-inputFile = "C:/Users/gagm2737/Documents/SpiderOak Hive/DONNEES_FILAIRES/WORKINPROGRESS/outputDebitmetre.csv"
-outputFile = "C:/Users/gagm2737/Documents/SpiderOak Hive/DONNEES_FILAIRES/WORKINPROGRESS/output_convertedToEventBased.csv"
+inputFile = "C:/Users/gagm2737/Documents/SpiderOak Hive/DONNEES_FILAIRES/06. DEBITMETRES BINARISES/outputDebitmetre30janv2017.csv"
+outputFile = "C:/Users/gagm2737/Documents/SpiderOak Hive/DONNEES_FILAIRES/WORKINPROGRESS/output_convertedToEventBased30janv2017.csv"
 
-allData = read.csv(inputFile)
+allData <- read.csv(inputFile)
 
 #Vector containing all EXP names
 expList <- c('EXP18', 'EXP21', 'EXP22', 'EXP23', 'EXP24', 'EXP25', 'EXP26', 'EXP27', 'EXP28', 'EXP29', 'EXPDTA16', 'EXPDTA17',
              'EXPDTA18', 'EXPDTA19', 'EXPDTA20', 'EXPMCI16', 'EXPMCI17', 'EXPMCI18', 'EXPMCI19', 'EXPMCI20', 'EXPMCI21',
              'EXPMCI23', 'EXPMCI24', 'EXPMCI25', 'EXPMCI26')
 
-#Declare endData, a data frame containing final exit data (nb trigger of all sensors for all EXPs)
-endData <- data.frame(matrix(ncol = 4, nrow = length(expList)))
-colnames(endData) <- c('No_Test', 'sensor', 'statut', 'heure')
+#Declare endData, a data frame containing final output data
+endData <- data.frame(matrix(ncol = 7, nrow = length(expList)))
+colnames(endData) <- c('No_Test', 'sensor', 'status', 'elapsed', 'hours', 'minutes', 'seconds')
 
+#sensor value 
 previous = 0
 current = 0
+
+#variables for the time
+hours = 0
+minutes = 0
+seconds = 0
 
 counter = 1
 
 #Loop over all EXPs
 for (indexExp in 1:length(expList)){
   #Extract data for the current EXP
-  expData = allData[grep(expList[indexExp], allData$No_Test),]
+  expData <- allData[grep(expList[indexExp], allData$No_Test),]
   
   #for each sensors (excluding first column (No_Test))
   for (indexSensor in 2:length(expData[1,])){
@@ -31,15 +37,24 @@ for (indexExp in 1:length(expList)){
       previous <- expData[indexLine - 1, indexSensor]
       current <- expData[indexLine, indexSensor]
 
-      #instead, write info in new data
+      #check if an event (change of value) has occured
       if (current != previous){
-        #print(c("AAAAAAAAAAAAAAAAAAA ", indexSensor, indexLine))
 
+        #convert elapsed time into hours, minutes and seconds
+        #reminder : indexLine represents the number of seconds elapsed
+        hours = trunc(indexLine / 3600)
+        minutes = trunc(indexLine / 60) - (hours*60)
+        seconds = indexLine - (minutes*60) - (hours*3600)
+
+        sensorName = colnames(expData)[indexSensor]
+        
         tuple <- c(expList[indexExp],
-                   colnames(expData)[indexSensor],
+                   sensorName,
                    current,
-                   indexLine
-                   )
+                   indexLine,
+                   hours,
+                   minutes,
+                   seconds)
 
         endData[counter,] <- tuple
         counter = counter + 1
@@ -49,3 +64,6 @@ for (indexExp in 1:length(expList)){
   }
   
 }
+
+#Output in csv format
+write.csv(endData, file = outputFile, row.names = FALSE )
--- standardizeFlowmetre.R
+++ standardizeFlowmetre.R
@@ -1,6 +1,6 @@
 #Data Frame containing all data
 inputFile = "C:/Users/gagm2737/Documents/SpiderOak Hive/DONNEES_FILAIRES/05. APRÈS filtres/AllEXPs-27janv2017-apresFiltres.csv"
-outputFile = "C:/Users/gagm2737/Documents/SpiderOak Hive/DONNEES_FILAIRES/06. DEBITMETRES BINARISES/outputDebitmetre27janv2017.csv"
+outputFile = "C:/Users/gagm2737/Documents/SpiderOak Hive/DONNEES_FILAIRES/06. DEBITMETRES BINARISES/outputDebitmetre30janv2017.csv"
 
 allData = read.csv(inputFile)
 expList <- c('EXP18', 'EXP21', 'EXP22', 'EXP23', 'EXP24', 'EXP25', 'EXP26', 'EXP27', 'EXP28', 'EXP29', 'EXPDTA16', 'EXPDTA17',
@@ -19,13 +19,42 @@ for (i in 1:length(expList)){
   #Extract data for the current EXP
   expData = allData[grep(expList[i], allData$No_Test),]
   dbSensorList = grep("DB", colnames(expData[,2:length(expData[1,])]), value = TRUE)
-
-  for (dbSensor in dbSensorList){
-    for (j in 1:length(expData[[dbSensor]])){
+  mvSensorList = grep("MV", colnames(expData[,2:length(expData[1,])]), value = TRUE)
+  ccSensorList = grep("C", colnames(expData[,2:length(expData[1,])]), value = TRUE)
+  
+#code original fonctionnel pour dbSensor
+  # for (dbSensor in dbSensorList){
+  #   for (j in 1:length(expData[[dbSensor]])){
+  #     if (expData[[dbSensor]][j] >= 10) {expData[[dbSensor]][j] <- 1}
+  #   }
+  # }
+  
+  #for all lines of data in the EXP
+  for (j in 1:length(expData[,1])){
+    for (dbSensor in dbSensorList){
       if (expData[[dbSensor]][j] >= 10) {expData[[dbSensor]][j] <- 1}
     }
+    
+    #invert values of movement sensors
+    for (mvSensor in mvSensorList){
+      if (expData[[mvSensor]][j] == 1) {
+        expData[[mvSensor]][j] <- 0
+      } else{
+        expData[[mvSensor]][j] <- 1
+      }
+    }
+    
+    #invert values of contact sensors
+    for (ccSensor in ccSensorList){
+      if (expData[[ccSensor]][j] == 1) {
+        expData[[ccSensor]][j] <- 0
+      } else{
+        expData[[ccSensor]][j] <- 1
+      }
+    }
+    
   }
-  
+
   #écrire dans fichier
   write.table(expData, file = outputFile, 
               row.names = FALSE, col.names = FALSE, append = TRUE, qmethod = "double", dec = ".", sep = "," )
